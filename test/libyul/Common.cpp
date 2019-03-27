c2b5623135b70d46fd05d0c66d9ad516ca7fa25a
chriseth <chris@ethereum.org>
2019-03-27 12:49:50 +0100

    Make optimiser settings available to assembly stack.

--- test/libyul/Common.cpp
+++ test/libyul/Common.cpp
@@ -66,7 +66,8 @@ pair<shared_ptr<Block>, shared_ptr<yul::AsmAnalysisInfo>> yul::test::parse(strin
 {
 	AssemblyStack stack(
 		dev::test::Options::get().evmVersion(),
-		_yul ? AssemblyStack::Language::Yul : AssemblyStack::Language::StrictAssembly
+		_yul ? AssemblyStack::Language::Yul : AssemblyStack::Language::StrictAssembly,
+		dev::test::Options::get().optimize ? OptimiserSettings::standard() : OptimiserSettings::minimal()
 	);
 	if (!stack.parseAndAnalyze("", _source) || !stack.errors().empty())
 		BOOST_FAIL("Invalid source.");
